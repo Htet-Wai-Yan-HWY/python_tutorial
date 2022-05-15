72075d1dc0c958fc5fa88f85a9e43e6c973f1b59
Zyme <100279699+Htet-Wai-Yan-HWY@users.noreply.github.com>
2022-05-15 21:40:59 +0630

    Create zero_one.py

---
+++ zero_one.py
@@ -0,0 +1,40 @@
+# arr = [];
+# output = 0;
+# final = [];
+# var = 0;
+
+# for i in chaa:
+#     arr.append(i)
+# for j in chaa:
+#     if j == "0":
+#         output=output+1
+        
+#     if j=="1":
+#         output=0
+    
+
+# print(output)    
+
+chaa =  "1001101000110"
+x=len(chaa)
+for i in range (0,x):
+    print(chaa[i],end=".")
+
+
+
+# final = 0
+# test = 0
+
+# for i in chaa:
+#     if i == "0":
+#         test += 1
+#     else:
+#         if test > final:
+#             final = test
+#         test = 0
+
+# if test > final:
+#     final = test
+# print(final)
+
+#print(chaa.split("1"))
